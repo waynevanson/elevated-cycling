5aec9d3e1f451282ffc7c616a466d096e6d5b5ed
Wayne Van Son <waynevanson@gmail.com>
2024-01-17 22:16:46 +1100

    chore: rustofy

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,19 @@ FROM openelevation/open-elevation as open-elevation
 WORKDIR /code
 RUN ./download-srtm-data.sh
 RUN ./create-dataset.sh
+RUN command: stdbuf -i0 -o0 -e0 python3 server.python3
 
 # Download planet.osm.pbf for map data that we can traverse.
+# If you already have this file you can skip the download and copy the file to `/data.osm.pbf`.
+# https://download.geofabrik.de/
 FROM debian as planet-osm
 RUN apt-get update
-RUN apt-get install -y curl
-RUN curl 'https://download.bbbike.org/osm/planet/planet-latest.osm.pbf' -O '/data.osm.pbf'
+RUN apt-get install -y wget
+RUN \
+    wget 'https://download.geofabrik.de/australia-oceania/australia-latest.osm.pbf' \
+    --output-document '/data/map.osm.pbf' \
+    --no-verbose --show-progress --progress=dot:giga:noscroll \
+    --continue 
 
 FROM rust as server-build
 WORKDIR /app/
@@ -18,7 +25,8 @@ RUN \
     cargo build --release elevation
 
 FROM rust as server
+WORKDIR /app
 VOLUME [ "/data", "/app"]
-CMD cargo watch -x run --release elevation -- /data.osm.pbf
+CMD cargo watch -x run --release -- /data/map.osm.pbf
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,48 @@
+use axum::{handler::Handler, response::Json, routing::get, Router};
+use clap::Parser;
+use osmpbf::ElementReader;
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+
+// Get the file path at startup via CLI invocation
+#[derive(Debug, Clone, Parser)]
+struct Args {
+    file_path: PathBuf,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+struct CircuitDownHillRequest {
+    latitude: f32,
+    longitude: f32,
+    radius: u8,
+}
+
+#[derive(Debug, Clone, Serialize)]
+struct CircuitDownHillResponse {
+    coordinates: Vec<(f32, f32)>,
+}
+
 #[tokio::main]
-async fn main() {}
+async fn main() {
+    let file_path = Args::parse().file_path;
+
+    // build our application with a route
+    let app = Router::new().route(
+        "/",
+        get(|Json(request): Json<CircuitDownHillRequest>| async {
+            let reader = ElementReader::from_path(file_path).unwrap();
+            Json(CircuitDownHillResponse {
+                coordinates: vec![],
+            })
+        }),
+    );
+
+    // run it
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
+    println!("listening on {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app).await.unwrap();
+}
